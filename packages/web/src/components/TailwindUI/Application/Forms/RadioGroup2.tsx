9c8d2b55aa42b6ee1adc8b8817b3a110c8b2e399
Ricky <Ricky@1737270443@qq.com>
2022-04-13 17:14:05 +0800

    feat(web): ✨ 修复bug

--- packages/web/src/components/TailwindUI/Application/Forms/RadioGroup2.tsx
+++ packages/web/src/components/TailwindUI/Application/Forms/RadioGroup2.tsx
@@ -1,4 +1,7 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
+
+import type { FC } from 'react'
+
 /* eslint-disable prettier/prettier */
 const notificationMethods = [
   { id: 'email', title: 'Email' },

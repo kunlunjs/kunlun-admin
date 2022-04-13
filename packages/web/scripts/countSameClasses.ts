e196d9cfc0e484c7b0c32df3dcd231557e20b68d
Yudong Zhu <zhuyudong@aliyun.com>
2022-04-13 11:06:47 +0800

    feat(web): ✨ 增加同类型class出现的次数

---
+++ packages/web/scripts/countSameClasses.ts
@@ -0,0 +1,40 @@
+// 统计相同的类名最小出现的次数
+const min = 5
+
+;(async () => {
+  const { readFileSync, writeFileSync } = await import('fs')
+  const { resolve } = await import('path')
+  const { globbySync } = await import('globby')
+  const paths = globbySync(
+    resolve(__dirname, '../src/components/TailwindUI/**/*.tsx')
+  )
+
+  const counters: Record<string, number> = {}
+  paths.forEach(i => {
+    const classes = readFileSync(i)
+      .toString()
+      .match(/className="([a-zA-Z-\d\s:]+)"/g)
+      ?.map(i => i.slice(11, -1))
+    classes?.forEach(j => {
+      if (counters[j]) {
+        counters[j] = counters[j] + 1
+      } else {
+        counters[j] = 1
+      }
+      // counters[j] += counters[j] ?? 1
+    })
+  })
+  writeFileSync(
+    resolve(__dirname, 'count.json'),
+    JSON.stringify(
+      Object.entries(counters)
+        .filter(i => i[1] >= min)
+        .reduce((a, c) => {
+          a[c[0]] = c[1]
+          return a
+        }, {} as Record<string, number>),
+      null,
+      2
+    )
+  )
+})()
